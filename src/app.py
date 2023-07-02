8012746172bfd6f34f3993c362505f6ca34a016c
romeB92 <59175913+romeB92@users.noreply.github.com>
2023-07-02 20:36:15 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -8,7 +8,9 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Starships
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required,JWTManager
+
 #from models import Person
 
 app = Flask(__name__)
@@ -26,6 +28,9 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -36,14 +41,128 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#       !!--USER--!!
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_user():
+   
 
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
 
     return jsonify(response_body), 200
+
+#       !!--LOGIN--!!
+@app.route('/login', methods=['POST'])
+def handle_login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({
+            "msg": "No account was found. Please check the email used or create an account."
+        }), 401
+    
+    if password != user.password:
+        return jsonify({"msg": "Incorrect password. Please try again."}), 401
+
+    access_token = create_access_token(identity=email)
+
+    payload = {"token":access_token, "msg": "Login successful"}
+    return jsonify(payload), 200
+
+#       !!--SIGN-UP--!!
+@app.route('/signup', methods=['POST'])
+def handle_signup():
+    body = request.json # get the request body content
+    email = request.json.get('email')
+    name = request.json.get('name')
+
+    password = request.json.get('password')
+    
+    if body is None:
+        return "The request body is null", 400
+    if not email:
+        return 'You need to enter an email',400
+    if not name:
+        return 'You need to enter an name',400
+    if not password:
+        return 'You need to enter a password', 400
+    check_user = User.query.filter_by(email=email).first()
+
+    if check_user is not None:
+        return jsonify({
+            'msg': 'The email address already exists. Please login to your account to continue.'
+        }),409
+
+    user = User(email=email, name=name, password=password, is_active=True)
+
+    db.session.add(user)
+    db.session.commit()
+   
+    payload = {
+        'msg': 'Your account has been registered successfully.',
+        'user': user.serialize()
+    }
+
+    return jsonify(payload), 200
+
+# get all characters
+@app.route('/characters', methods=['GET'])
+def getAllCharacters():
+  characters = Characters.query.all()
+  if characters is None:
+    return jsonify(msg="This page does not exist")
+  else:
+    return jsonify(data=[character.serialize() for character in characters]) 
+
+# get one Character
+@app.route('/characters/<int:id>', methods=['GET'])
+def getOneCharacters(id):
+  character = Characters.query.get(id)
+  if character is None:
+    return jsonify(msg="This page does not exist")
+  else:
+    return jsonify(data=character.serialize())
+
+# get all planets
+@app.route('/planets', methods=['GET'])
+def getAllPlanets():
+  planets = Planets.query.all()
+  if planets is None:
+    return jsonify(msg="This page does not exist")
+  else:
+    return jsonify(data=[planet.serialize() for planet in planets]) 
+
+# get one planet
+@app.route('/planets/<int:id>', methods=['GET'])
+def getOnePlanet(id):
+  planet = Planets.query.get(id)
+  if planet is None:
+    return jsonify(msg="This page does not exist")
+  else:
+    return jsonify(data=planet.serialize())
+
+# get all starships
+@app.route('/starships', methods=['GET'])
+def getAllStarships():
+  starships = Starships.query.all()
+  if starships is None:
+    return jsonify(msg="This page does not exist")
+  else:
+    return jsonify(data=[starship.serialize() for starship in starships]) 
+
+# get one starship
+@app.route('/starships/<int:id>', methods=['GET'])
+def getOneStarships(id):
+  starship = Starship.query.get(id)
+  if starship is None:
+    return jsonify(msg="This page does not exist")
+  else:
+    return jsonify(data=starship.serialize())
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,128 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
+
+favorite_characters = db.Table(
+    "favorite_characters",
+
+    db.Column("user_id", db.ForeignKey("user.id")),
+    db.Column("character_id", db.ForeignKey("characters.id")),
+)
+
+favorite_planets = db.Table(
+    "favorite_planets",
+   
+    db.Column("user_id", db.ForeignKey("user.id")),
+    db.Column("planet_id", db.ForeignKey("planets.id")),
+)
+favorite_starships = db.Table(
+    "favorite_starships",
+
+    db.Column("user_id", db.ForeignKey("user.id")),
+    db.Column("starship_id", db.ForeignKey("starships.id")),
+)
+    
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String())
+
+    favorite_characters = db.relationship('Characters',secondary=favorite_characters)
+    favorite_planets = db.relationship('Planets',secondary=favorite_planets)
+    favorite_starships = db.relationship('Starships',secondary=favorite_starships)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            "favorite_characters": list(map(lambda x: x.serialize(), self.favorite_characters)),
+            "favorite_planets": list(map(lambda x: x.serialize(), self.favorite_planets)),
+            "favorite_starships": list(map(lambda x: x.serialize(), self.favorite_starships))
+            # do not serialize the password, its a security breach
+        }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(250), nullable=False)
+    homeworld = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "homeworld": self.homeworld,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+    name = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    gravity = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "gravity": self.gravity,
+            "climate": self.climate,
+            # do not serialize the password, its a security breach
+        }
+    
+class Starships(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    model = db.Column(db.String(250), nullable=False)
+    starship_class = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False)
+    length = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Starship %r>' % self.model
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "model": self.model,
+            "starship_class": self.starship_class,
+            "manufacturer": self.manufacturer,
+            "length": self.length,
             # do not serialize the password, its a security breach
         }
